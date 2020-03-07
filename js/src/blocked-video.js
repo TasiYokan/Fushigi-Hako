9525d80d51b6b6a4cf1e98129a3d653984af73b2
TasiYokan <guanghao.yang@outlook.com>
2020-03-07 18:08:48 +0800

    Deploy

--- js/src/blocked-video.js
+++ js/src/blocked-video.js
@@ -41,6 +41,8 @@ function WebLocationCheck() {
     }
 }
 
-window.onload = function () {
-    WebLocationCheck();
-}
+// window.onload = function () {
+//     WebLocationCheck();
+// }
+
+WebLocationCheck();
